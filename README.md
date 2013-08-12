4f5ac52448d09baf3e74bd146e6388092fc11cfd
jtong <tj19832@gmail.com>
2013-08-12 18:13:09 +0800

    jtong - fix founded component issue

--- README.md
+++ README.md
@@ -27,3 +27,7 @@ TODO: Write usage instructions here
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
 5. Create new Pull Request
+
+TODO
+1. header_left
+2. header_right
--- lib/moode-haml-component-angular.rb
+++ lib/moode-haml-component-angular.rb
@@ -1,27 +1,31 @@
 require "moode-haml-component-angular/version"
 
-template_path = File.expand_path('../moode-haml-component-angular', __FILE__)
+class  Object < BasicObject
 
-haml_files = File.join(template_path, "*.haml")
-Dir.glob haml_files do |file|
-  file_name = File.basename file , ".haml"
-  puts file_name
-  method_name = file_name[1, file_name.length].to_sym
-  if(not self.respond_to? method_name)
-    self.define_singleton_method method_name do |args = {}|
-      template = load_component_template(method_name)
-      Haml::Engine.new(template).render Object.new, args
+  template_path = File.expand_path('../moode-haml-component-angular', __FILE__)
+
+  haml_files = File.join(template_path, "*.haml")
+  Dir.glob haml_files do |file|
+    file_name = File.basename file , ".haml"
+    method_name = file_name[1, file_name.length].to_sym
+    if(not self.respond_to? method_name, true)
+      self.send :define_method, method_name do |args = {}|
+        template = File.read("#{template_path}/_#{method_name}.haml")
+        Haml::Engine.new(template).render Object.new, args
+      end
     end
   end
-end
 
-containers_files = File.join(template_path, "container", "*.haml")
-Dir.glob containers_files do |file|
-  file_name = File.basename file , ".haml"
-  method_name = file_name[1, file_name.length].to_sym
-  if(not self.respond_to? method_name)
-    self.define_singleton_method method_name do |args = {}, &block|
-      container "component/container/#{method_name}.haml", args, &block
+  containers_files = File.join(template_path, "container", "*.haml")
+  Dir.glob containers_files do |file|
+    file_name = File.basename file , ".haml"
+    method_name = file_name[1, file_name.length].to_sym
+    if(not self.respond_to? method_name)
+      self.send :define_method, method_name do |args = {}, &block|
+        container "#{template_path}/container/_#{method_name}.haml", args, &block
+      end
     end
   end
+
+
 end
